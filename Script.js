ccc1bfb72952582db0691af25988529a66e75eb4
Hernan-github <arenivas.soto0@gmail.com>
2026-05-06 14:09:46 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,38 @@
+function validarAnagrama(){
+    const palabra1 = document.getElementById("palabra1").value.toLowerCase().replace(/\s/g, '');
+    const palabra2 = document.getElementById("palabra2").value.toLowerCase().replace(/\s/g, '');
+
+    const sorted1 = palabra1.split('').sort().join('');
+    const sorted2 = palabra2.split('').sort().join('');
+    if (sorted1 === sorted2) {
+        alert("¡Las palabras son anagramas!");
+    } else {
+        alert("Las palabras no son anagramas.");
+    }
+    
+}
+
+function crearBurbuja() {
+    const bubble = document.createElement("div");
+    bubble.classList.add("bubble");
+
+    // Tamaño aleatorio
+    const size = Math.random() * 40 + 10;
+    bubble.style.width = size + "px";
+    bubble.style.height = size + "px";
+
+    // Posición horizontal aleatoria
+    bubble.style.left = Math.random() * window.innerWidth + "px";
+
+    // Duración aleatoria
+    bubble.style.animationDuration = (Math.random() * 5 + 3) + "s";
+
+    document.getElementById("bubbles").appendChild(bubble);
+
+    // Eliminar burbuja después de animación
+    setTimeout(() => {
+        bubble.remove();
+    }, 8000);
+}
+
+setInterval(crearBurbuja, 200); 
